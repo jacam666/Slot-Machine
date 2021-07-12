2884a7efaf09f39cf0c5993bb98e9298e8b2743f
jacam666 <ja6cam@gmail.com>
2021-07-12 12:48:30 +0100

    : couldn't get the code(gridnumbergenerator/ gridresult) to repeat itself while it was in a method, so took them out from the methods(for now), : added diagonal result lines

--- Slot Machine/Program.cs
+++ Slot Machine/Program.cs
@@ -17,11 +17,9 @@ namespace Slot_Machine
             Console.WriteLine($"Welcome to my slot game " + playerName);
             Console.Write("Amount of Cash you would like to use in the slots: £");
             double cash = Convert.ToDouble(Console.ReadLine());
-            string response = null;
-            
-
-            int[,] gridNumbers = GridNumberGenerator();
-            GridResult(gridNumbers);
+            string response;
+            //int[,] gridNumbers = GridNumberGenerator();
+            //GridResult(gridNumbers);
             for (; ; ) // Loop forever
             {
                 if (cash <= 0)
@@ -40,95 +38,134 @@ namespace Slot_Machine
                 else
                 {
                     cash -= 1;
+
+                    int[,] gridNumbers = new int[3, 3];
+                    Random rng = new();
+                    for (int i = 0; i < gridNumbers.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < gridNumbers.GetLength(1); j++)
+                        {
+                            gridNumbers[i, j] = rng.Next(0, 6);
+                            Console.Write(gridNumbers[i, j] + " ");
+                        }
+                        Console.WriteLine(" ");
+                    }
+
+                    for (int i = 0; i < gridNumbers.GetLength(0); i++)
+                    {
+                        if (gridNumbers[i, 0] == gridNumbers[i, 1] && gridNumbers[i, 1] == gridNumbers[i, 2])
+                        {
+                            Console.WriteLine("you win");
+                            cash += 2;
+                        }
+                        if (gridNumbers[0, i] == gridNumbers[1, i] && gridNumbers[1, i] == gridNumbers[2, i])
+                        {
+                            Console.WriteLine("you win");
+                            cash += 2;
+                        }
+                       
+                    }// adding diagonal winning lines.
+                    if ((gridNumbers[0, 0]) == (gridNumbers[1, 1]) && (gridNumbers[1, 1]) == (gridNumbers[2, 2]))
+                    {
+                        Console.WriteLine("Congrats You Win");
+                        cash += 5;
+                    }
+                    if ((gridNumbers[0, 2]) == (gridNumbers[1, 1]) && (gridNumbers[1, 1]) == (gridNumbers[2, 0]))
+                    {
+                        Console.WriteLine("Congrats You Win");
+                        cash += 5;
+                    }
+
                 }
             }
 
-            static int[,] GridNumberGenerator()
-            {
-                int[,] gridNumbers = new int[3, 3];
-                Random rng = new();
-                for (int i = 0; i < gridNumbers.GetLength(0); i++)
-                {
-                    for (int j = 0; j < gridNumbers.GetLength(1); j++)
-                    {
-                        gridNumbers[i, j] = rng.Next(0, 3);
-                        Console.Write(gridNumbers[i, j] + " ");
-                    }
-                    Console.WriteLine(" ");
-                }
+            //static int[,] GridNumberGenerator()
 
-                return gridNumbers;
+            //{
+            //int[,] gridNumbers = new int[3, 3];
+            //Random rng = new();
+            //for (int i = 0; i < gridNumbers.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < gridNumbers.GetLength(1); j++)
+            //    {
+            //        gridNumbers[i, j] = rng.Next(0, 3);
+            //        Console.Write(gridNumbers[i, j] + " ");
+            //    }
+            //    Console.WriteLine(" ");
+            //}
 
-            }
+            //return gridNumbers;
 
-            static void GridResult(int[,] gridNumbers)
-            {    // matching vertical line.
+            //}
 
-                for (int i = 0; i < gridNumbers.GetLength(0); i++)
-                {
-                    if (gridNumbers[i, 0] == gridNumbers[i, 1] && gridNumbers[i, 1] == gridNumbers[i,2])
-                    {
-                        Console.WriteLine("you win");
-                    }
-                    if (gridNumbers[0, i] == gridNumbers[1, i] && gridNumbers[1, i] == gridNumbers[2, i])
-                    {
-                        Console.WriteLine("you win");
-                    }
-                }
+            //static void GridResult(int[,] gridNumbers)
+            //{    // matching vertical line.
 
-                
-
+            //for (int i = 0; i < gridNumbers.GetLength(0); i++)
+            //{
+            //    if (gridNumbers[i, 0] == gridNumbers[i, 1] && gridNumbers[i, 1] == gridNumbers[i,2])
+            //    {
+            //        Console.WriteLine("you win");
+            //    }
+            //    if (gridNumbers[0, i] == gridNumbers[1, i] && gridNumbers[1, i] == gridNumbers[2, i])
+            //    {
+            //        Console.WriteLine("you win");
+            //    }
+            //}
 
 
-                //int verticlReel1 = gridNumbers[0, 0] + gridNumbers[0, 1] + gridNumbers[0, 2];
-                //int verticlReel2 = gridNumbers[1, 0] + gridNumbers[1, 1] + gridNumbers[1, 2];
-                //int verticlReel3 = gridNumbers[2, 0] + gridNumbers[2, 1] + gridNumbers[2, 2];
-
-                    //int horizontalReel1 = gridNumbers[0, 0] + gridNumbers[1, 0] + gridNumbers[2, 0];
-                    //int horizontalReel2 = gridNumbers[0, 1] + gridNumbers[1, 1] + gridNumbers[2, 1];
-                    //int horizontalReel3 = gridNumbers[0, 2] + gridNumbers[1, 2] + gridNumbers[2, 2];
-
-                    //int diadonalReel1 = gridNumbers[0, 0] + gridNumbers[1, 1] + gridNumbers[2, 2];
-                    //int diagonalReel3 = gridNumbers[2, 0] + gridNumbers[1, 5] + gridNumbers[0, 3];
-
-                    //if ((gridNumbers[0, 0]) == (gridNumbers[0, 1]) && (gridNumbers[0, 1]) == (gridNumbers[0, 2]))
-                    //{
-                    //    Console.WriteLine("Congrats You Win");
-                    //}
-                    //if ((gridNumbers[1, 0]) == (gridNumbers[1, 1]) && (gridNumbers[1, 1]) == (gridNumbers[1, 2]))
-                    //{
-                    //    Console.WriteLine("Congrats You Win");
-                    //}
-                    //if ((gridNumbers[2, 0]) == (gridNumbers[2, 1]) && (gridNumbers[2, 1]) == (gridNumbers[2, 2]))
-                    //{
-                    //    Console.WriteLine("Congrats You Win");
-                    //}
-
-                    //// matching vertical lines
-                    //if ((gridNumbers[0, 0]) == (gridNumbers[1, 0]) && (gridNumbers[1, 0]) == (gridNumbers[2, 0]))
-                    //{
-                    //    Console.WriteLine("congrats You Win");
-                    //}
-                    //if ((gridNumbers[0, 1]) == (gridNumbers[1, 1]) && (gridNumbers[1, 1]) == (gridNumbers[2, 1]))
-                    //{
-                    //    Console.WriteLine("Congrats You Win");
-                    //}
-                    //if ((gridNumbers[0, 2]) == (gridNumbers[1, 2]) && (gridNumbers[1, 2]) == (gridNumbers[2, 2]))
-                    //{
-                    //    Console.WriteLine("Congrats You Win");
-                    //}
-                    //// matching diagonal lines
-                    //if ((gridNumbers[0, 0]) == (gridNumbers[1, 1]) && (gridNumbers[1, 1]) == (gridNumbers[2, 2]))
-                    //{
-                    //    Console.WriteLine("Congrats You Win");
-                    //}
-                    //if ((gridNumbers[0, 2]) == (gridNumbers[1, 1]) && (gridNumbers[1, 1]) == (gridNumbers[2, 0]))
-                    //{
-                    //    Console.WriteLine("Congrats You Win");
-                    //}
 
 
-            }
+
+            //int verticlReel1 = gridNumbers[0, 0] + gridNumbers[0, 1] + gridNumbers[0, 2];
+            //int verticlReel2 = gridNumbers[1, 0] + gridNumbers[1, 1] + gridNumbers[1, 2];
+            //int verticlReel3 = gridNumbers[2, 0] + gridNumbers[2, 1] + gridNumbers[2, 2];
+
+            //int horizontalReel1 = gridNumbers[0, 0] + gridNumbers[1, 0] + gridNumbers[2, 0];
+            //int horizontalReel2 = gridNumbers[0, 1] + gridNumbers[1, 1] + gridNumbers[2, 1];
+            //int horizontalReel3 = gridNumbers[0, 2] + gridNumbers[1, 2] + gridNumbers[2, 2];
+
+            //int diadonalReel1 = gridNumbers[0, 0] + gridNumbers[1, 1] + gridNumbers[2, 2];
+            //int diagonalReel3 = gridNumbers[2, 0] + gridNumbers[1, 5] + gridNumbers[0, 3];
+
+            //if ((gridNumbers[0, 0]) == (gridNumbers[0, 1]) && (gridNumbers[0, 1]) == (gridNumbers[0, 2]))
+            //{
+            //    Console.WriteLine("Congrats You Win");
+            //}
+            //if ((gridNumbers[1, 0]) == (gridNumbers[1, 1]) && (gridNumbers[1, 1]) == (gridNumbers[1, 2]))
+            //{
+            //    Console.WriteLine("Congrats You Win");
+            //}
+            //if ((gridNumbers[2, 0]) == (gridNumbers[2, 1]) && (gridNumbers[2, 1]) == (gridNumbers[2, 2]))
+            //{
+            //    Console.WriteLine("Congrats You Win");
+            //}
+
+            //// matching vertical lines
+            //if ((gridNumbers[0, 0]) == (gridNumbers[1, 0]) && (gridNumbers[1, 0]) == (gridNumbers[2, 0]))
+            //{
+            //    Console.WriteLine("congrats You Win");
+            //}
+            //if ((gridNumbers[0, 1]) == (gridNumbers[1, 1]) && (gridNumbers[1, 1]) == (gridNumbers[2, 1]))
+            //{
+            //    Console.WriteLine("Congrats You Win");
+            //}
+            //if ((gridNumbers[0, 2]) == (gridNumbers[1, 2]) && (gridNumbers[1, 2]) == (gridNumbers[2, 2]))
+            //{
+            //    Console.WriteLine("Congrats You Win");
+            //}
+            //// matching diagonal lines
+            //if ((gridNumbers[0, 0]) == (gridNumbers[1, 1]) && (gridNumbers[1, 1]) == (gridNumbers[2, 2]))
+            //{
+            //    Console.WriteLine("Congrats You Win");
+            //}
+            //if ((gridNumbers[0, 2]) == (gridNumbers[1, 1]) && (gridNumbers[1, 1]) == (gridNumbers[2, 0]))
+            //{
+            //    Console.WriteLine("Congrats You Win");
+            //}
+
+
+            //}
 
 
 
